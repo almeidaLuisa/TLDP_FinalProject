d2ed90a07af63c523463e215a8a3b79b42b613da
Luisa <luisalmeida0106@gmail.com>
2025-12-01 15:44:06 -0500

    testing

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_db():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
-
+#testing something 
 def init_db():
     conn = get_db()
     cursor = conn.cursor()
